ea5d2fb6922bf90ca67772aa876d387216547058
Dhruv Charkhawala <dv.charkhawala@gmail.com>
2023-12-19 12:55:54 +0530

    project 1 and 2 changes

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,24 +1,22 @@
-import { useState } from 'react';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [counter,setCounter] = useState(1)
-  // var counter=0;
-  const addValue=()=>{
-    setCounter(counter+1)
-  }
-
-  const removeValue=()=>{
-    setCounter(counter-1);
-  }
+  const [counter,setCounter] = useState(0);
 
   return (
     <>
       <h1>Counter Project</h1>
-      <button onClick={removeValue}>-</button>{"   "}
-      <button >{counter}</button>{"   "}
-      <button onClick={addValue}>+</button>
-
+      <h3>Counter Value : {counter}</h3>
+      <button onClick={()=>{
+        setCounter(counter+1);
+      }}> Add Value</button>
+      <button onClick={()=>{
+        setCounter(counter-1);
+      }}>remove value</button>
+      <p>footer : {counter}</p>
     </>
   )
 }
